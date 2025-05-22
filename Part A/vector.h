a8699b27e8b1f57b86a667aa1db97169af36ba0d
Aqikop <115913406+Aqikop@users.noreply.github.com>
2025-05-22 20:12:16 +0700

    Create vector.h

---
+++ Part A/vector.h
@@ -0,0 +1,38 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector{
+    private:
+        int mSize;
+        double* mData;
+    public:
+        // Constructor and Destructor
+        Vector();
+        Vector(int size);
+        Vector(const Vector& other);
+        ~Vector();
+
+        Vector& operator=(const Vector& other); // Assignment operator
+
+        // Unary operators
+        Vector operator-() const;
+        Vector operator+() const;
+        Vector operator++();
+        Vector operator--();
+
+        // Binary operators
+        Vector operator+(const Vector& other) const;
+        Vector operator-(const Vector& other) const;
+        Vector operator*(double scalar) const;
+        double operator*(const Vector& other) const; // Dot product(v₁·v₂ = a×d + b×e + c×f )
+
+        double& operator[](int index);
+        double& operator()(int index);
+
+        int get_size() const{
+            return mSize;
+        };
+};
+Vector operator*(double scalar, const Vector& other); // Scalar multiplication (v₁×a = a×v₁ = [a×b, a×c, a×d])
+
+#endif
