6840816fb1cf75ab38b9230b516e73ee572e0316
Nam <khanhnam176@gmail.com>
2025-05-27 13:41:20 +0000

    test 2

--- Part A/possymlinsystem.cpp
+++ Part A/possymlinsystem.cpp
@@ -7,11 +7,34 @@
 #include <math.h>
 using namespace std;
 
-Vector PosSymLinSystem::Solve(Vector x0 = Vector(mpb->mSize)) {
+Vector PosSymLinSystem::Solve(Vector x0 = Vector(mpb->mSize), int max_iters = 100) {
     //not implement yet
-    Vector r0;
-    r0 = mpb - mpA*x0
-    //
+    Vector r0, p0, x0_temp, r0_temp;
+    double r0_dot, alpha, beta;
+    x0_temp = x0;
+    r0 = mpb - mpA*x0;
+    p0 = r0;
+    //sol temp
+    int iters = 0;
+    while(iters < max_iters){
+        //saving old residue
+        r0_temp = r0;
+        //step size
+        r0_dot = r0*r0;  //r0.T * r0
+        alpha = r0_dot / (mpA*r0*r0); // r0_dot / r0.T * mpA * r0
+        //update solution
+        x0 = x0_temp + alpha*p0; // x0_temp + alpha * p0
+        x0_temp = x0;
+        //update residue
+        r0 = mpb - mpA*x0;
+        iters += 1
+        //check stop
+        //compute if |r0| < epsilon (very small)
+        //if no
+        beta = (r0*r0) / (r0_temp*r0_temp);
+        p0 = r0 + beta*p0;
+    }
+    return x0;
 }
 
 bool PosSymLinSystem::Check() {
