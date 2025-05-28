425720253edd1d646d70bfa0bcd4aed8e0214931
Nam <khanhnam176@gmail.com>
2025-05-28 08:50:14 +0000

    done, no test

--- Part A/possymlinsystem.cpp
+++ Part A/possymlinsystem.cpp
@@ -7,7 +7,7 @@
 #include <math.h>
 using namespace std;
 
-Vector PosSymLinSystem::Solve(Vector x0 = Vector(mpb->mSize), int max_iters = 100) {
+Vector PosSymLinSystem::Solve(Vector x0 = Vector(mpb->mSize), int max_iters = 100, double min = 0.1) {
     //not implement yet
     Vector r0, p0, x0_temp, r0_temp;
     double r0_dot, alpha, beta;
@@ -21,7 +21,7 @@ Vector PosSymLinSystem::Solve(Vector x0 = Vector(mpb->mSize), int max_iters = 10
         r0_temp = r0;
         //step size
         r0_dot = r0*r0;  //r0.T * r0
-        alpha = r0_dot / (mpA*r0*r0); // r0_dot / r0.T * mpA * r0
+        alpha = r0_dot / ((mpA*p0)*p0); // r0_dot / r0.T * mpA * r0
         //update solution
         x0 = x0_temp + alpha*p0; // x0_temp + alpha * p0
         x0_temp = x0;
@@ -30,9 +30,12 @@ Vector PosSymLinSystem::Solve(Vector x0 = Vector(mpb->mSize), int max_iters = 10
         iters += 1
         //check stop
         //compute if |r0| < epsilon (very small)
-        //if no
-        beta = (r0*r0) / (r0_temp*r0_temp);
+        //r0.length < min => break;
+        if(r0.length() < min){break;}
+        else{
+            beta = (r0*r0) / (r0_temp*r0_temp);
         p0 = r0 + beta*p0;
+        }
     }
     return x0;
 }
