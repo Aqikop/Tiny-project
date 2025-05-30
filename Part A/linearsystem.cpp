0633c0e33d7b7c7b97b075d864a304fa69cb21e0
Nam <khanhnam176@gmail.com>
2025-05-30 19:35:19 +0700

    Update linearsystem.cpp

--- Part A/linearsystem.cpp
+++ Part A/linearsystem.cpp
@@ -9,46 +9,54 @@ using namespace std;
 
 LinearSystem::LinearSystem(Matrix* A, Vector* b) : mpA(A), mpb(b) {
     mSize = A->getNumRows();
-    assert(A->getNumRows() == A->getNumCols());
+    assert(A->getNumRows() == A->getNumCols());//may need remove
 }
 //Solve Method: (Reference: Copilot)
 Vector LinearSystem::Solve() {
-    int n = mSize;
-    vector<vector<double>> a(n, vector<double>(n));
-    Vector b = *mpb;
-    for (int i = 0; i < n; ++i) {
-        b[i] = (*mpb)[i];
-        for (int j = 0; j < n; ++j) {
-            a[i][j] = (*mpA)(i+1, j+1); // 1-based indexing in Matrix
-        }
-    }
-    // Gaussian elimination with partial pivoting
-    for (int i = 0; i < n; ++i) {
-        int pivot = i;
-        for (int j = i+1; j < n; ++j) {
-            if (fabs(a[j][i]) > fabs(a[pivot][i])) pivot = j;
-        }
-        assert(fabs(a[pivot][i]) > 1e-12);
-        if (pivot != i) {
-            swap(a[i], a[pivot]);
-            swap(b[i], b[pivot]);
-        }
-        for (int j = i+1; j < n; ++j) {
-            double factor = a[j][i] / a[i][i];
-            for (int k = i; k < n; ++k) {
-                a[j][k] -= factor * a[i][k];
+    if(mpA->getNumRows() == mpA->getNumCols()){
+        int n = mSize;
+        vector<vector<double>> a(n, vector<double>(n));
+        Vector b = *mpb;
+        for (int i = 0; i < n; ++i) {
+            b[i] = (*mpb)[i];
+            for (int j = 0; j < n; ++j) {
+                a[i][j] = (*mpA)(i+1, j+1); // 1-based indexing in Matrix
             }
-            b[j] -= factor * b[i];
         }
-    }
-    // Back substitution
-    Vector res(n);
-    for (int i = n-1; i >= 0; --i) {
-        res[i] = b[i];
-        for (int j = i+1; j < n; ++j) {
-            res[i] -= a[i][j] * res[j];
+        // Gaussian elimination with partial pivoting
+        for (int i = 0; i < n; ++i) {
+            int pivot = i;
+            for (int j = i+1; j < n; ++j) {
+                if (fabs(a[j][i]) > fabs(a[pivot][i])) pivot = j;
+            }
+            assert(fabs(a[pivot][i]) > 1e-12);
+            if (pivot != i) {
+                swap(a[i], a[pivot]);
+                swap(b[i], b[pivot]);
+            }
+            for (int j = i+1; j < n; ++j) {
+                double factor = a[j][i] / a[i][i];
+                for (int k = i; k < n; ++k) {
+                    a[j][k] -= factor * a[i][k];
+                }
+                b[j] -= factor * b[i];
+            }
         }
-        res[i] /= a[i][i];
+        // Back substitution
+        Vector res(n);
+        for (int i = n-1; i >= 0; --i) {
+            res[i] = b[i];
+            for (int j = i+1; j < n; ++j) {
+                res[i] -= a[i][j] * res[j];
+            }
+            res[i] /= a[i][i];
+        }
+        return res;
     }
-    return res;
+    else if(mpA->getNumRows() > mpA->getNumCols()){
+        //for overdetermined
+    }
+    else{
+        //for underdetermined
+    }
 }
